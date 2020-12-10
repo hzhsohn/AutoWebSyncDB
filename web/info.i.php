6565ea2114225cd6c6273e91f8b50b1717e8213c
han.zh <hzh.888@qq.com>
2020-12-10 18:17:59 +0800

    新建数据判断接口 info.i.php
    Signed-off-by: han.zh <hzh.888@qq.com>

---
+++ web/info.i.php
@@ -0,0 +1,48 @@
+﻿<?php
+/**************************************
+
+Author:Han.zhihong
+remark:
+数据显示页
+
+***************************************/
+
+require_once("module/mysql.m.php");
+require_once("_config.php");
+
+$scan_key=$_SERVER['QUERY_STRING'];
+if(0==strcmp($scan_key,''))
+{
+  echo 'no parameter';
+	exit;
+}
+
+$db=new PzhMySqlDB();		
+$db->open_mysql(cfg_db_host,cfg_db,cfg_db_username,cfg_db_passwd);
+$db->query("select*from tb_board where scan_key='$scan_key'",0,0);
+if($rs=$db->read())
+{
+	//var_dump($rs);
+ 	$j=json_decode($rs['json'],true);
+}
+if(null==$j)
+{
+	echo 'not found scan key';
+	exit;
+}
+//var_dump($j);
+
+$db->query("select*from tb_append where scan_key='$scan_key'",0,0);
+$l=array();
+while($rs=$db->read())
+{
+ 	$l[]=json_decode($rs['json'],true);
+}
+$db->close();
+//var_dump($l);
+
+
+$j['list']=$l;
+echo json_encode($j);;
+
+?>
